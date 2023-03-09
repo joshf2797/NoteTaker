e189f23cbdf0940750e0bda011f7cc6df256e294
Joshua <joshf2797@gmail.com>
2023-03-08 22:52:16 -0700

    finished up the server, got it to work, just need to add the styling and other functionalities

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const path = require('path');
 const fs = require('fa');
 const uuid = require('uuid');
 
-const PORT = process.env.PORT || 3001:
+
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 // Use middlware for parsing JSON and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-use.express(express.static('public'));
+app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-    const note = JSON.parse(fs.readFileSync('.db/db.json'));
+app.get('/api/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, './db/db.json'))
+);
+
+app.post('/api/notes', (req, res) => {
+const note = JSON.parse(fs.readFileSync('.db/db.json'));
     const {title, text} = req.body;
 
     if (title && text) {
@@ -34,3 +40,23 @@ app.get('/api/notes', (req, res) => {
     fs.writeFileSync('.db/db.json', JSON.stringify(note), "utf-8");
     res.json(note);
 });
+
+// to delete note
+app.delete('/api/notes/:id', (req, res) => {
+    const deleteNote = note.filter((delNote) => delNote.id !== req.params.id);
+    fs.writeFileSync('.db/db.json', JSON.stringify(deleteNote));
+    res.json(deleteNote);
+});
+
+// routing
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+
+app.listen(PORT, () => {
+    console.log('Now Listening')
+})
