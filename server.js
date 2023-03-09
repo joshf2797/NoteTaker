533fd9c87153fd55540f91ed32e7d405e7d46ede
Joshua <joshf2797@gmail.com>
2023-03-08 18:33:30 -0700

    added more code to server.js

--- server.js
+++ server.js
@@ -11,3 +11,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 use.express(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+    const note = JSON.parse(fs.readFileSync('.db/db.json'));
+    const {title, text} = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+        note.push(newNote);
+
+        const response = {
+            status: 'success',
+            bosy: newNote,
+        };
+        res.status(207).json(response);
+    } else {
+        res.status(500).json('Cannot Post Note');
+    }
+    fs.writeFileSync('.db/db.json', JSON.stringify(note), "utf-8");
+    res.json(note);
+});
